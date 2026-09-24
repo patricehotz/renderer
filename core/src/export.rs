303dff41356315bb319072b8ef07370a9a4d4a44
Patrice Hotz <94618682+patricehotz@users.noreply.github.com>
2026-09-24 22:49:35 +0200

    First fucking gradiant juhuuugit add .!

--- core/src/export.rs
+++ core/src/export.rs
@@ -1,15 +1,17 @@
 use crate::{Buffer, Coordinate, RGBA};
 use std::fmt::Write;
 
-pub fn ppm(buffer: &Buffer, dir: String) {
-    let mut value = format!("p3\n{} {}\n255\n", buffer.width, buffer.height);
+pub fn ppm(buffer: &Buffer, dir: &str) {
+    let mut value = format!("P3\n{} {}\n255\n", buffer.width, buffer.height);
     for y in 0..buffer.height {
-        for x in 0..buffer.width-1 {
+        for x in 0..buffer.width {
             let RGBA {r, g, b, ..} = buffer.get_pixel(Coordinate{x, y});
-            write!(value, "{} {} {}", r, g, b).unwrap()
+            write!(value, "{} {} {} ", r, g, b).unwrap()
         }
-        if (y < buffer.height-1) {
+        if y < buffer.height-1 {
             writeln!(value).unwrap()
         }
     };
+
+    std::fs::write(dir, value).unwrap()
 }
--- core/src/lib.rs
+++ core/src/lib.rs
@@ -7,12 +7,17 @@ pub struct Coordinate {
     pub y: usize
 }
 
+pub struct Vec2 {
+    pub x: f32,
+    pub y: f32,
+}
+
 #[derive(Clone, Copy)]
 pub struct RGBA {
-    r: u8,
-    g: u8,
-    b: u8,
-    a: u8,
+    pub r: u8,
+    pub g: u8,
+    pub b: u8,
+    pub a: u8,
 }
 
 pub struct Buffer {
@@ -23,7 +28,7 @@ pub struct Buffer {
 
 impl Buffer {
     pub fn new(width: usize, height: usize) -> Self {
-        Self {width, height, pixels: Vec::new()}
+        Self {width, height, pixels: vec![RGBA{r: 255, g: 255, b: 255, a: 100}; width * height ]}
     }
 
     pub fn get_pixel(&self, Coordinate {x, y}: Coordinate) -> RGBA {
